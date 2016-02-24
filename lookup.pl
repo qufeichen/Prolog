7deaa25654ca897ffdc5d9085dd679362bd518e9
Qufei Chen <qufei.chen@gmail.com>
2016-02-24 14:34:04 -0500

    element lookup

---
+++ lookup.pl
@@ -0,0 +1,27 @@
+% lookup.
+% interactive program that takes in a periodic element symbol and returns the name of the element
+
+% sample db
+element(chlorine,'Cl').
+element(helium,'He').
+element(hydrogen,'H').
+element(nitrogen,'N').
+element(oxygen,'O').
+
+% lookup
+lookup:- repeat,
+	write('Symbol to look-up: '),
+	read(X),
+	eval(Y,X).
+
+eval(Y,X):- element(Y,X),
+	write(X),
+	write(' is the symbol for: '),
+	writeln(Y),
+	!.
+
+eval(Y,X):-
+	write('Dont know the symbol for: '),
+	writeln(X),
+	writeln('Exiting.'),
+	!.
